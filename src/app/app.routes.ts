26d276bbf847a06a270627f43857d61beacc9225
Ridan77 <dan.ribak17@gmail.com>
2026-01-07 12:11:58 +0200

    add products components and routes (single routing file)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
-export  const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+
+  { path: 'products', component: ProductsListComponent },
+  { path: 'products/new', component: ProductFormComponent },
+  { path: 'products/:id', component: ProductFormComponent },
+
+  { path: '**', redirectTo: 'products' },
+];
+
